2acdd1b6f96221093980aa471c4ca3a27733ce54
noahzhy <e-it@foxmail.com>
2020-01-18 18:10:18 +0800

    update

--- chapter_02_mnist.py
+++ chapter_02_mnist.py
@@ -1,6 +1,15 @@
+import os
 from keras.datasets import mnist
+from keras import models
+from keras import layers
 
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-print(len(train_images), train_images.shape)
-print(train_labels)
+# 加载数据集
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data(os.path.join(os.getcwd(), 'datasets/mnist.npz'))
+print('train labels:', train_labels, len(train_images), train_images.shape)
+print('test labels: ', test_labels, len(test_images), test_labels.shape)
+
+# 网络架构
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
+network.add(layers.Dense(10, activation='softmax'))
