fc73165f2c0cff182753088c7aa467f34cde3039
noahzhy <e-it@foxmail.com>
2020-01-18 17:14:01 +0800

    upload datasets

--- chapter_02_mnist.py
+++ chapter_02_mnist.py
@@ -1,5 +1,6 @@
 from keras.datasets import mnist
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-print(len(train_images))
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+print(len(train_images), train_images.shape)
+print(train_labels)
