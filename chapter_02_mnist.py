0ed386d849cefc1590f4fec8ee26e0664ab89952
noahzhy <noahzhy@gmail.com>
2020-01-27 21:12:23 +0800

    更新注释

--- chapter_02_mnist.py
+++ chapter_02_mnist.py
@@ -15,19 +15,27 @@ network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
 network.add(layers.Dense(10, activation='softmax'))
 
+# 编译
 network.compile(
     optimizer='rmsprop',
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-train_images = train_images.reshape((6000, 28*28))
+# 预处理图像数据
+train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
 
-test_images = test_images.reshape((6000, 28*28))
+test_images = test_images.reshape((10000, 28*28))
 test_images = test_images.astype('float32') / 255
 
+# 准备标签
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
+# 拟合
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+# 评估
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('test_acc', test_acc)
